a90210805c84b18518c9fa57360089723a51247f
Andrey <Fian.123@yandex.ru>
2026-03-23 18:16:52 +0300

    add NoteList screen

---
+++ app/src/main/java/com/yourname/taskmanager/screen/new_note_screen/NewNoteEvent.kt
@@ -0,0 +1,13 @@
+package com.yourname.taskmanager.screen.new_note_screen
+
+sealed class NewNoteEvent {
+    data class OnTitleChange(
+        val title: String
+    ): NewNoteEvent()
+
+    data class OnDescriptionChange(
+        val description: String
+    ): NewNoteEvent()
+
+    object OnSave: NewNoteEvent()
+}
