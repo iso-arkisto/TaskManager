45ca59042b3fb3e0fabeee12d826d4fb206dda97
Andrey <Fian.123@yandex.ru>
2026-06-06 20:45:00 +0300

    feat: create AddItemScreen UI layout and connect with ViewModel

--- app/src/main/java/com/yourname/taskmanager/screen/add_item_screen/AddItemScreen.kt
+++ app/src/main/java/com/yourname/taskmanager/screen/add_item_screen/AddItemScreen.kt
@@ -1,8 +1,156 @@
 package com.yourname.taskmanager.screen.add_item_screen
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.yourname.taskmanager.R
+import com.yourname.taskmanager.dialog.MainDialog
+import com.yourname.taskmanager.ui.theme.BlueLight
+import com.yourname.taskmanager.ui.theme.DarkText
+import com.yourname.taskmanager.ui.theme.GrayLightSoft
+import com.yourname.taskmanager.ui.theme.PurpleGrey40
+import com.yourname.taskmanager.utils.UIEvent
+import kotlinx.coroutines.flow.flowOf
 
 @Composable
-fun AddItemScreen() {
+fun AddItemScreen(
+    viewModel: AddItemViewModel = hiltViewModel()
+) {
+    val snackbarHostState = remember { SnackbarHostState() }
+    val itemsList = (viewModel.itemsList ?: flowOf(emptyList())).collectAsState(emptyList())
 
+    LaunchedEffect(key1 = true) {
+        viewModel.uiEvent.collect { uiEvent ->
+            when(uiEvent){
+                is UIEvent.ShowSnackBar -> {
+                    snackbarHostState.showSnackbar(
+                        uiEvent.text
+                    )
+                }
+                else -> {}
+            }
+        }
+    }
+
+    Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState) { snackbarData ->
+                Snackbar(
+                    snackbarData = snackbarData,
+                    containerColor = PurpleGrey40,
+                    modifier = Modifier.padding(bottom = 24.dp)
+                )
+            }
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .systemBarsPadding()
+                .background(GrayLightSoft)
+                .padding(paddingValues)
+
+        ) {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(5.dp)
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    TextField(
+                        modifier = Modifier.weight(1f),
+                        value = viewModel.itemText.value,
+                        onValueChange = { text: String ->
+                            viewModel.onEvent(AddItemEvent.OnTextChange(text))
+                        },
+                        label = {
+                            Text(
+                                text = "New item",
+                                fontSize = 12.sp
+                            )
+                        },
+                        colors = TextFieldDefaults.colors(
+                            focusedIndicatorColor = BlueLight,
+                            unfocusedIndicatorColor = Color.Transparent,
+                            disabledIndicatorColor = Color.Transparent,
+                            focusedContainerColor = Color.White,
+                            unfocusedContainerColor = Color.White,
+                        ),
+                        textStyle = androidx.compose.ui.text.TextStyle(
+                            fontSize = 16.sp,
+                            color = DarkText,
+                        ),
+                        singleLine = true
+                    )
+                    IconButton(onClick = {
+                        viewModel.onEvent(AddItemEvent.OnItemSave)
+                    }) {
+                        Icon(
+                            painter = painterResource(
+                                R.drawable.baseline_add_24
+                            ),
+                            contentDescription = "Add"
+                        )
+                    }
+                }
+            }
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(start = 5.dp, end = 5.dp)
+            ) {
+                items(itemsList.value){ item ->
+                    UiAddItem(item, onEvent = { event ->
+                        viewModel.onEvent(event)
+                    })
+                }
+            }
+
+            if(itemsList.value.isEmpty()){
+                Text(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .wrapContentHeight(),
+                    text = "Empty",
+                    textAlign = TextAlign.Center,
+                    fontSize = 25.sp,
+                    color = Color.LightGray
+                )
+            }
+        }
+        MainDialog(viewModel)
+    }
 }
